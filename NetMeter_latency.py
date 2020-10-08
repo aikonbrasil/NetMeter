b2ed3e1acc64c6c89eb110dac4c2d46918f24622
Dick Carrillo <dcarrillom@gmail.com>
2020-10-08 10:56:37 +0300

    adding new dev version of latency

--- NetMeter_latency.py
+++ NetMeter_latency.py
@@ -463,7 +463,7 @@ def get_iperf_data_single(iperf_out, protocol, streams, repetitions):
               print('time to End')
               print(time_from_end)
 
-              if (time_from_end-time_from_start) != 10 #Interval greater than 10 sec.
+              if ((time_from_end-time_from_start) != 10): #Interval greater than 10 sec.
                   print('paso ok aqui ')
                   iperf_data.append([time_from_start, lat_average, lat_stand, id_stream])
 
