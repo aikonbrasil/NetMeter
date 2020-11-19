87c6f013dadc7a15a1f7fb72198a1aec34322811
Dick Carrillo <dcarrillom@gmail.com>
2020-11-19 11:22:50 +0200

    Update download_packages.sh

--- pacman/download_packages.sh
+++ pacman/download_packages.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 ARCH="armv7l"
-MIRROR="https://mirror.pseudoform.org/"
+MIRROR="dk.mirror.archlinuxarm.org"
 
-wget "${MIRROR}/community/os/${ARCH}/community.db"
-wget "${MIRROR}/core/os/${ARCH}/core.db"
-wget "${MIRROR}/extra/os/${ARCH}/extra.db"
-wget "${MIRROR}/multilib/os/${ARCH}/multilib.db"
+
+wget "${MIRROR}/armv7h/community/community.db"
+wget "${MIRROR}/armv7h/core/core.db"
+wget "${MIRROR}/armv7h/extra/extra.db"
+wget "${MIRROR}/armv7h/alarm/alarm.db"
+wget "${MIRROR}/armv7h/aur/aur.db"
